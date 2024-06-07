d63a7a2d4b8093e40153c09679d7dc9a96bfb902
whxvmi <64111097+whxvmi@users.noreply.github.com>
2024-06-07 13:22:59 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 <URL>"
+    echo "Usage: $0 <URL or File>"
     exit 1
 fi
 
-url="$1"
-location=$(curl -v --silent -POST http://127.0.0.1:1337/v0.1/scan  2>&1 \
-    -H "Content-Type: application/json" \
-    -d '{"urls": ["'"$url"'"]}' | grep Location | cut -d ':' -f2- | tr -d '[:space:]')
+if [[ -f "$1" ]]; then
+    urls=()
+    while IFS= read -r line; do
+        urls+=("$line")
+    done < "$1"
+    location=$(curl -v --silent -POST http://127.0.0.1:1337/v0.1/scan  2>&1 \
+        -H "Content-Type: application/json" \
+        -d '{"urls": '"$(printf '%s\n' "${urls[@]}" | jq -R . | jq -s .)"'}' | grep Location | cut -d ':' -f2- | tr -d '[:space:]')
+else
+    url="$1"
+    location=$(curl -v --silent -POST http://127.0.0.1:1337/v0.1/scan  2>&1 \
+        -H "Content-Type: application/json" \
+        -d '{"urls": ["'"$url"'"]}' | grep Location | cut -d ':' -f2- | tr -d '[:space:]')
+fi
 
 echo "Launched. TaskID: $location"
-
-while true; do
-    sleep 5
-    curl -X GET --url "http://localhost:1337/v0.1/scan/$location"
-done
+sleep 5
+curl -X GET --url "http://localhost:1337/v0.1/scan/$location"
